0382b6d6b0ab7d4c665811925f9386f6ac798454
Nasiruddeen Farhan Ahmad <48054276+Farnas2019@users.noreply.github.com>
2022-03-24 03:36:46 -0700

    Delete keys.js

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // db_URL:"mongodb://localhost:27017/chops",
-     db_URL:"mongodb+srv://ice669:07066944154@cluster0.oghz8.mongodb.net/chopspot?retryWrites=true&w=majority",
-    APP_SECRET: "idunuthedeveloper"
-}
